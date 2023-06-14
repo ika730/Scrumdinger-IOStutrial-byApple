3e83a29f0eac6c1460b6e5a5b28536c297defe27
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-06-14 17:26:16 +0900

    Initial Commit

---
+++ Scrumdinger-iOStutrial-byApple/Scrumdinger_iOStutrial_byAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_iOStutrial_byAppleApp.swift
+//  Scrumdinger-iOStutrial-byApple
+//
+//  Created by 中川こうすけ on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_iOStutrial_byAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
