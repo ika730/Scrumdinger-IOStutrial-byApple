fd216449595eed270dd829cdc322ca2f2011e372
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-06-14 17:57:14 +0900

    section2終了

--- Scrumdinger-iOStutrial-byApple/Scrumdinger_iOStutrial_byAppleApp.swift
+++ Scrumdinger-iOStutrial-byApple/Scrumdinger_iOStutrial_byAppleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Scrumdinger_iOStutrial_byAppleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
